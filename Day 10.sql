ba111d094df40ca178522064ca6d226be9ce9a67
Mag LA <magdalenelawsons@gmail.com>
2025-12-10 20:23:10 +0000

    Update Day 10.sql

--- Day 10.sql
+++ Day 10.sql
@@ -1,3 +1,9 @@
+/* 
+https://www.hackerrank.com/challenges/occupations
+Corrections made 
+Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output should consist of four columns (Doctor, Professor, Singer, and Actor) in that specific order, with their respective names listed alphabetically under each column.
+Note: Print NULL when there are no more names corresponding to an occupation.
+*/
 with doctor as (
 Select 
         name,
